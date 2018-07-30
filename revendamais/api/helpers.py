82ed90ded5ef7fec90d73865acfb2d03274228b4
Lucas Souto <lucasinfologos@gmail.com>
2018-07-29 22:44:24 -0300

    Fixbug query search

--- revendamais/api/helpers.py
+++ revendamais/api/helpers.py
@@ -1,4 +1,6 @@
 import json
+import requests
+import re
 import twitter
 from config.settings.base import (API_KEY,
                                   API_SECRET,
@@ -29,13 +31,13 @@ class Twitter:
 
             return list_trends
 
-    def search(self, term=None, raw_query=None):
-        return self.api.GetSearch(
-            term=term,
-            count=20,
-            raw_query=raw_query,
-            return_json=True
-        )
+    def search(self, term='', raw_query=None):
+        if term != '':
+            term = requests.utils.unquote(term)
+        if raw_query is not None:
+            raw_query = re.sub('[^a-zA-Z0-9=&_-]', '', raw_query)
+
+        return self.api.GetSearch(term=term, count=20, raw_query=raw_query, return_json=True)
 
     def locations(self):
         with open(FILE_WOEID, 'r') as f:
